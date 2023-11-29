8f1d1061d263a7cae274a013649434979c7f291b
Denis Kalkopf <deniskalkopf@gmail.com>
2023-11-29 11:37:44 +0500

    add GateWayClass

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -6,15 +6,36 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { AppService } from '../app.service';
+import { Prisma } from '@prisma/client';
 
 @WebSocketGateway()
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(private appService: AppService) {}
+
   @WebSocketServer() server: Server;
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    client: Socket,
+    payload: Prisma.ChatCreateInput,
+  ): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: any) {
+    console.log(server);
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
   }
 }
