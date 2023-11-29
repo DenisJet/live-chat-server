392251d20538dffadd10c6b7d70b02e049109905
Denis Kalkopf <deniskalkopf@gmail.com>
2023-11-29 11:26:24 +0500

    prisma i

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Chat, Prisma } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createMessage(data: Prisma.ChatCreateInput): Promise<Chat> {
+    return await this.prisma.chat.create({ data });
+  }
+
+  async getMessages(): Promise<Chat[]> {
+    return await this.prisma.chat.findMany();
   }
 }
--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,7 +1,18 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class AppGateway {
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
